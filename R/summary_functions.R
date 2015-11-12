37ab7f1d31ba3a97f0700efeddce6b9629180f1d
jimmylovestea <jmolyneux@ucla.edu>
2015-11-11 23:34:06 -0800

    Update mosaic fav_stats function

--- R/summary_functions.R
+++ R/summary_functions.R
@@ -165,3 +165,16 @@ var <- function(x, ...) {
   return(output)
 }
 
+fav_stats <- function (x, ..., na.rm = TRUE) {
+  if (!is.null(dim(x)) && min(dim(x)) != 1)
+    warning("Not respecting matrix dimensions.  Hope that's OK.")
+  x <- as.vector(x)
+  qq <- stats::quantile(x, na.rm = na.rm)
+  val <- data.frame(qq[1], qq[2], qq[3], qq[4], qq[5], base::mean(x,
+                                                                  na.rm = na.rm), stats::sd(x, na.rm = na.rm), base::sum(!is.na(x)),
+                    base::sum(is.na(x)))
+  rownames(val) <- ""
+  names(val) <- c("min", "Q1", "median", "Q3", "max", "mean",
+                  "sd", "n", "missing")
+  return(val)
+}
