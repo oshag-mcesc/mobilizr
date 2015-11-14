ec13fc5adcd1cfd6293325cdf535f200d062dbb8
jimmylovestea <jmolyneux@ucla.edu>
2015-11-14 13:02:42 -0800

    Remove old functions

--- R/mad.R
+++
@@ -1,9 +0,0 @@
-MAD.simple <- function(x) {
-    nas <- length(which(is.na(x)))
-    n <- length(x)
-    not_na <- n - nas
-    mad <- SAD.simple(x) / not_na
-    return(mad)
-}
-
-MAD <- aggregatingFunction1(MAD.simple)
