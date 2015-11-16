ba8c27f6939645bb3cded189418aa8580087591e
jimmylovestea <jmolyneux@ucla.edu>
2015-11-16 11:06:08 -0800

    bloopbloopbloop

--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
 data <- function(...) {
   args <- list(...)
   exist <- length(args)
-  utils::data(...)
   if (exist != 0) {
     if (is.null(args[['package']])) {
       View(...)
     }
   }
+  utils::data(...)
 }
