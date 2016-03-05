80bf57b40cf4c886324fcf80bdca1c8aaf7376e6
jimmylovestea <jmolyneux@ucla.edu>
2016-03-05 09:16:53 -0800

    Adjustment to the add_line function

--- R/add_line.R
+++ R/add_line.R
@@ -5,15 +5,15 @@ add_line <- function(intercept, slope, units = "native") {
     ind1_raw <- grid.locator(unit = "native")
     ind2_raw <- grid.locator(unit = "native")
     ind1 <- as.numeric(ind1_raw)
-    ind2 <- as.numeric(ind2_raw)    
+    ind2 <- as.numeric(ind2_raw)
     m1 <- (ind2[2] - ind1[2])/(ind2[1] - ind1[1])
     b1 <- ind1[2] - m1 * ind1[1]
     cat("Equation of the line: \n")
     if (b1 >= 0) cat("y = ", m1, "* x +", b1)
     if (b1 < 0) cat("y = ", m1, "* x -", abs(b1))
     trellis.unfocus()
-    mosaic::ladd(panel.abline(a = b1, b = m1), data = list(b1 = b1, m1 = m1))
+    ladd(panel.abline(a=b1, b=m1))
   } else {
-    mosaic::ladd(panel.abline(a = a, b = b), data = list(b = slope, a = intercept))
+    ladd(panel.abline(a=intercept, b=slope, col = "red"))
   }
 }
