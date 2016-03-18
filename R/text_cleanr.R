87c651d111ff1121e677f884728e8f09637c8304
jimmylovestea <jmolyneux@ucla.edu>
2016-03-17 22:15:15 -0700

    Update to maureen's function

--- R/text_cleanr.R
+++ R/text_cleanr.R
@@ -1,7 +1,14 @@
 text_cleanr <- function(insert_original_texting_data_here) {
   bye_bye_brackets <- insert_original_texting_data_here %>%
     mutate(Birth_Gender = gsub("\\[|\\]", "", Birth_Gender))
+  bye_bye_spaces <- bye_bye_brackets %>%
+    mutate(Birth_Gender = gsub(" ", "", Birth_Gender))
 
   print("Stop texting in math class!")
-  return(bye_bye_brackets)
+  return(bye_bye_spaces)
 }
+
+again <- mobilizr::read.csv("~/Downloads/Fixed data.csv")
+
+again <- text_cleanr(again)
+str(again)
