248ef2ccbd44cc73c4b8c85ff1140ec0f5c20f5f
jimmylovestea <jmolyneux@ucla.edu>
2016-11-01 01:08:55 -0700

    Bandaids stop bleeding

--- R/xsummarise.R
+++ R/xsummarise.R
@@ -1,42 +1,11 @@
-xsummarise <- function(.data, ...) {
-  stopifnot(is.data.frame(.data) || is.list(.data) || is.environment(.data))
-  cols <- as.list(substitute(list(...))[-1])
-  if (is.null(names(cols))) {
-    missing_names <- rep(TRUE, length(cols))
-  }
-  else {
-    missing_names <- names(cols) == ""
-  }
-  if (any(missing_names)) {
-    names <- unname(unlist(lapply(match.call(expand.dots = FALSE)$...,
-                                  deparse)))
-    names(cols)[missing_names] <- names[missing_names]
-  }
-  .data <- as.list(.data)
-  for (col in names(cols)) {
-    .data[[col]] <- eval(cols[[col]], .data, parent.frame())
-  }
-  quickdf(.data[names(cols)])
+summarise <- function(...) {
+  dplyr::summarise(...)
 }
 
-xsummarize <- function(.data, ...) {
-  stopifnot(is.data.frame(.data) || is.list(.data) || is.environment(.data))
-  cols <- as.list(substitute(list(...))[-1])
-  if (is.null(names(cols))) {
-    missing_names <- rep(TRUE, length(cols))
-  }
-  else {
-    missing_names <- names(cols) == ""
-  }
-  if (any(missing_names)) {
-    names <- unname(unlist(lapply(match.call(expand.dots = FALSE)$...,
-                                  deparse)))
-    names(cols)[missing_names] <- names[missing_names]
-  }
-  .data <- as.list(.data)
-  for (col in names(cols)) {
-    .data[[col]] <- eval(cols[[col]], .data, parent.frame())
-  }
-  quickdf(.data[names(cols)])
+summarize <- function(...) {
+  dplyr::summarise(...)
 }
 
+rename <- function(...) {
+  dplyr::rename(...)
+}
