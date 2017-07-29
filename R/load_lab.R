e9672c5ff5c56b6efe37ef4c5dd0dc50ce5de6dc
James Molyneux <james@Jamess-MacBook-Air.local>
2017-07-29 02:56:07 -0700

    Update the load lab function

--- R/load_lab.R
+++ R/load_lab.R
@@ -13,6 +13,11 @@
 #' load_lab(1) # Automatically loads the 1st lab from the menu.
 #' load_labs() # Similar to load_lab()
 #' load_labs(1) # Similar to load_lab(1)
+#'
+#' @importFrom curl curl
+#' @importFrom rstudioapi viewer
+#' @importFrom stringr str_extract
+#' @export
 
 load_lab <- function(lab) {
   # Create the names of the labs for the menu
@@ -48,7 +53,7 @@ load_lab <- function(lab) {
   # and displays it in the Viewer pane.
 
   # Get HTML
-  con <- curl::curl(url, "r")
+  con <- curl(url, "r")
   page <- paste(readLines(con), collapse = '\n')
   close(con)
 
@@ -57,10 +62,11 @@ load_lab <- function(lab) {
   writeLines(page, tf)
 
   # Display HTML file in the viewer pane.
-  rstudioapi::viewer(tf)
+  viewer(tf)
 }
 
 #' @rdname load_lab
+#' @export
 load_labs <- function(lab) {
   # Alias to avoid problems with load_lab vs. load_labs in the written
   # curriculum
@@ -74,17 +80,26 @@ load_labs <- function(lab) {
   x_nospace <- gsub(x = x_lower, pattern = ' ', replacement = "")
   clean_title <- gsub(x = x_nospace, pattern = "-", replacement = "")
 
-  unit_num <- stringr::str_extract(clean_title, "[1-4]")
+  unit_num <- str_extract(clean_title, "[1-4]")
   lab_location <- paste0("unit_", unit_num, "/", clean_title, "/", clean_title)
   return(lab_location)
 }
 
+# Helper function that displays the actual menu to choose labs from.
 .lab_selector <- function (lab, lab_titles) {
-  # Helper function that displays the actual menu to choose labs from.
+
+  # Since LAUSD will be using the 'legacy' labs for an undiclosed length
+  # of time, we'll obtain Lab 1C from a different branch on github.
 
   # Create a list of URLs to choose from, 1 for each lab.
   lab_urls <- paste0('http://gh.mobilizingcs.org/ids_labs/',
                      .format_lab_title(lab_titles), '.html')
+
+  # Here is where we change which version of lab 1C is used for LAUSD
+  if (rstudioapi::getVersion() == "0.99.902") {
+    lab_urls[3] <- "https://raw.githubusercontent.com/mobilizingcs/ids_labs/lausd-labs/unit_1/lab1c/lab1c.html"
+  }
+
   if (is.null(lab)) {
     # If user doesn't specify a lab to open in load_lab(), prompt them.
     selection <- menu(lab_titles, title = "Enter the number next to the lab you would like to load:")
